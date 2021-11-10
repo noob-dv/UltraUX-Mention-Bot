3bd51edbdc6a2f850bf58e7b9e2208cd93a66c3e
Isane DV <92152410+noob-dv@users.noreply.github.com>
2021-11-10 10:03:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ async def mentionall(event):
     if not chat_id in spam_chats:
       break
     usrnum += 1
-    usrtxt += f"[{usr.fullname}](tg://user?id={usr.id}) "
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
     if usrnum == 7:
       if mode == "text_on_cmd":
         txt = f"{usrtxt}\n\n{msg}"
