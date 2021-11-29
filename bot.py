7b6663f30c7ae4d319c128c9ee025331fea1b116
Isane DV <92152410+noob-dv@users.noreply.github.com>
2021-11-29 07:24:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telethon.errors import UserNotParticipantError
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(name)s - [%(levelname)s] - %(message)s '
+    format='%(name)s - [%(levelname)s] - %(message)s'
 )
 LOGGER = logging.getLogger(__name__)
 
